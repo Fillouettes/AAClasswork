b3a59f096e584a941571c9fd1e290a5019c606f2
hernald <herndonald@gmail.com>
2020-07-29 11:51:27 -0400

    complete error_handling_fun

--- W4D2/beccaburten_error handling skeleton/super_useful.rb
+++ W4D2/beccaburten_error handling skeleton/super_useful.rb
@@ -1,37 +1,67 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+    begin 
+      Integer(str)
+    rescue ArgumentError => error
+      nil
+    end
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
-    puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+    if FRUITS.include? maybe_fruit
+      puts "OMG, thanks so much for the #{maybe_fruit}!"
+    elsif maybe_fruit == "coffee"
+      raise CoffeeError.new("Thanks for the coffee, but I want fruit!")
+    else
+      raise "That's not a fruit I like >:("
+    end
+end
+
+class CoffeeError < StandardError #if we give him coffee, try again for a fruit
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => error 
+    #raise StandardError  #if its neither fruit nor coffee, bigger error
+    puts error.message
+    retry
+  end 
+
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+    if name.length <= 0
+      raise "You don't even know me?"
+    else
+      @name = name
+    end
+    if yrs_known < 5
+      raise "You're no friend of mine!"
+    else
+      @yrs_known = yrs_known
+    end
+    if fav_pastime.length <= 0
+      raise "We don't do anything together"
+    else
+      @fav_pastime = fav_pastime
+    end
+
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known} years!"
   end
 
   def do_friendstuff
@@ -42,5 +72,3 @@ class BestFriend
     puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
   end
 end
-
-
--- W4D2/beccaburten_error handling skeleton/user_script.rb
+++ W4D2/beccaburten_error handling skeleton/user_script.rb
@@ -2,10 +2,29 @@ require_relative 'super_useful'
 
 puts "'five' == #{convert_to_int('five')}"
 
+
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('', 6, '') #=> name error
 
 sam.talk_about_friendship
 sam.do_friendstuff
 sam.give_friendship_bracelet
+
+don = BestFriend.new('bec', 70, '') #=> argument error OR pasttime error
+
+don.talk_about_friendship
+don.do_friendstuff
+don.give_friendship_bracelet
+
+joey = BestFriend.new('', 10, 'surfing') #=> name error
+
+joey.talk_about_friendship
+joey.do_friendstuff
+joey.give_friendship_bracelet
+
+real_pal = BestFriend.new('paloma', 20, 'coding') #=> no error
+
+real_pal.talk_about_friendship
+real_pal.do_friendstuff
+real_pal.give_friendship_bracelet
